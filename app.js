33abd05f46d47daa9d23ad88a16d8eef20dd1e2f
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-11-25 10:48:02 -0600

    finishing authentication

--- app.js
+++ app.js
@@ -18,21 +18,22 @@ app
     resave: false,
     saveUninitialized: true,
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', require('./routes/index'))
 app.use('/products', require('./routes/products'));
 app.use('/stores', require('./routes/stores'));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
     );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next();
 })
 app.use(cors({ origin: '*'}))
+
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -52,14 +53,15 @@ passport.deserializeUser((user, done) => {
 
 app.use(errorHandler);
 app.get('/', (req,res) => {
-    res.send(req.session.user !== undefined ? `Logged in as ${req.session.displayName}` : 'Logged Out') 
+    console.log(req.session.user);
+    res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') 
 })
 
 app.get('/github/callback', passport.authenticate('github', {
     failureRedirect: '/api-docs', session: false}), 
     (req, res) => {
     req.session.user = req.user;
-    res.redirect('/')
+    res.redirect('/');
 });
 
 mongodb.initDb((err) => {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const passport = require('passport');
 router.use('/', require('./swagger'));
-router.get('/login', passport.authenticate('github'));
+
+router.get('/login', passport.authenticate('github'), (req,res) => {});
 router.get('/logout', function(req,res,next) {
     req.logout(function(err) {
         if(err) {return next(err); }
